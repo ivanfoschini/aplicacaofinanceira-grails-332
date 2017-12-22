ea0c122a8a50db7743eb4c4dd5f2e82dd4993baa
Ivan Foschini <ivan@dc.ufscar.br>
2017-12-22 10:33:49 -0200

    Documentacao

--- grails-app/controllers/aplicacaofinanceirarestful/BancoController.groovy
+++ grails-app/controllers/aplicacaofinanceirarestful/BancoController.groovy
@@ -11,6 +11,59 @@ class BancoController {
     BancoService bancoService
     MessageSource messageSource
 
+    /**
+     * @api {delete} /banco/:id Delete
+     * @apiGroup Banco
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do banco.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 204 No Content
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484224334405,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/banco/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um banco que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "timestamp": 1484224424107,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Banco não encontrado.",
+     *       "path": "/banco/0"
+     *     }       
+     *
+     * @apiError Conflict O banco cujo identificador foi passado como parâmetro não pode ser excluído.
+     *
+     * @apiErrorExample Conflict
+     *     HTTP/1.1 409 Conflict
+     *     {
+     *       "timestamp": 1484224497234,
+     *       "status": 409,
+     *       "error": "Conflict",
+     *       "message": "O banco selecionado possui agências e, portanto, não pode ser excluído.",
+     *       "path": "/banco/1"
+     *     }
+     */
     def delete() {
         Banco banco = bancoService.findById(params.id as Long)
 
@@ -28,10 +81,208 @@ class BancoController {
         render status: HttpStatus.NO_CONTENT
     }
 
+    /**
+     * @api {get} /banco List
+     * @apiGroup Banco
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiSuccess {Number} id        Identificador do banco.
+     * @apiSuccess {Number} numero    Numero do banco.
+     * @apiSuccess {String} cnpj      CNPJ do banco.
+     * @apiSuccess {String} nome      Nome do banco.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     [
+     *       {
+     *         "id": 1,
+     *         "numero": 1,
+     *         "cnpj": "00000000000191",
+     *         "nome": "Banco do Brasil"
+     *       },
+     *       {
+     *         "id": 2,
+     *         "numero": 2,
+     *         "cnpj": "00360305000104",
+     *         "nome": "Caixa Econômica Federal"
+     *       },
+     *       {
+     *         "id": 3,
+     *         "numero": 3,
+     *         "cnpj": "60872504000123",
+     *         "nome": "Itaú"
+     *       }
+     *     ] 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484232706578,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/banco"
+     *     }
+     */
     def index() {
         respond bancoService.findAllOrderByNome()
     }
 
+     /**
+     * @api {post} /banco Save
+     * @apiGroup Banco
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} Content-Type  Tipo do conteúdo da requisição.
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "Content-Type":  application/json
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiExample {json} Exemplo de requisição
+     *     {
+     *       "numero": 1, 
+     *       "cnpj": "00000000000191",   
+     *       "nome": "Banco do Brasil"   
+     *     }
+     *
+     * @apiSuccess {Number} id      Identificador do banco.
+     * @apiSuccess {Number} numero  Numero do banco.
+     * @apiSuccess {String} cnpj    CNPJ do banco.
+     * @apiSuccess {String} nome    Nome do banco.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 201 Created
+     *     {
+     *       "id": 1,
+     *       "numero": 1,
+     *       "cnpj": "00000000000191",
+     *       "nome": "Banco do Brasil"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484224814226,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/banco"
+     *     }
+     *
+     * @apiError UnprocessableEntity Erros de validação de dados ou violação de regras de negócio.
+     * 
+     * @apiErrorExample UnprocessableEntity
+     *     HTTP/1.1 422 UnprocessableEntity - "Campos obrigatórios não foram fornecidos."
+     *     
+     *     {
+     *       "timestamp": 1484224887178,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido não pode ser nulo."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido não pode ser nulo."
+     *         },
+     *         {
+     *           "message": "O nome fornecido não pode ser nulo."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O número fornecido é menor do que zero."
+     *
+     *     {
+     *       "timestamp": 1484225003793,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido deve ser maior do que zero."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O número fornecido já pertence a outro banco."
+     *
+     *     {
+     *       "timestamp": 1484225086607,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido já pertence a outro banco."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido possui menos do que catorze caracteres."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido deve possuir 14 caracteres."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido possui mais do que catorze caracteres."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido deve possuir 14 caracteres."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido é inválido."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco"
+     *     }
+     */
     def save() {
         JsonSlurper jsonSlurper = new JsonSlurper()
         Banco banco = new Banco(jsonSlurper.parseText(request.JSON.toString()))
@@ -40,6 +291,58 @@ class BancoController {
         respond banco, [status: HttpStatus.CREATED, view: 'show']
     }
 
+    /**
+     * @api {get} /banco/:id Show
+     * @apiGroup Banco
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do banco.
+     *
+     * @apiSuccess {Number} id        Identificador do banco.
+     * @apiSuccess {Number} numero    Numero do banco.
+     * @apiSuccess {String} cnpj      CNPJ do banco.
+     * @apiSuccess {String} nome      Nome do banco.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "id": 1,
+     *       "numero": 1,
+     *       "cnpj": "00000000000191",
+     *       "nome": "Banco do Brasil"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484225396181,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/banco/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um banco que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "timestamp": 1484225446800,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Banco não encontrado.",
+     *       "path": "/banco/0"
+     *     }       
+     */
     def show() {
         Banco banco = bancoService.findById(params.id as Long)
 
@@ -50,6 +353,166 @@ class BancoController {
         }
     }
 
+    /**
+     * @api {put} /banco/:id Update
+     * @apiGroup Banco
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} Content-Type  Tipo do conteúdo da requisição.
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "Content-Type":  application/json
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do banco.
+     *
+     * @apiParamExample {json} Exemplo de requisição
+     *     {
+     *       "numero": 1, 
+     *       "cnpj": "00000000000191",   
+     *       "nome": "Banco do Brasil"   
+     *     }
+     *
+     * @apiSuccess {Number} id        Identificador do banco.
+     * @apiSuccess {Number} numero    Numero do banco.
+     * @apiSuccess {String} cnpj      CNPJ do banco.
+     * @apiSuccess {String} nome      Nome do banco.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "id": 1,
+     *       "numero": 1,
+     *       "cnpj": "00000000000191",
+     *       "nome": "Banco do Brasil"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484224814226,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/banco/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um banco que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "timestamp": 1484233284661,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Banco não encontrado.",
+     *       "path": "/banco/0"
+     *     }
+     *
+     * @apiError UnprocessableEntity Erros de validação de dados ou violação de regras de negócio.
+     * 
+     * @apiErrorExample UnprocessableEntity
+     *     HTTP/1.1 422 UnprocessableEntity - "Campos obrigatórios não foram fornecidos."
+     *     
+     *     {
+     *       "timestamp": 1484224887178,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido não pode ser nulo."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido não pode ser nulo."
+     *         },
+     *         {
+     *           "message": "O nome fornecido não pode ser nulo."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O número fornecido é menor do que zero."
+     *
+     *     {
+     *       "timestamp": 1484225003793,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido deve ser maior do que zero."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O número fornecido já pertence a outro banco."
+     *
+     *     {
+     *       "timestamp": 1484225086607,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O número fornecido já pertence a outro banco."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido possui menos do que catorze caracteres."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido deve possuir 14 caracteres."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido possui mais do que catorze caracteres."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido deve possuir 14 caracteres."
+     *         },
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O CNPJ fornecido é inválido."
+     *
+     *     {
+     *       "timestamp": 1484225163986,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O CNPJ fornecido é inválido."
+     *         }
+     *       ],
+     *       "path": "/banco/1"
+     *     }
+     */
     def update() {
         Banco banco = bancoService.findById(params.id as Long)
 
--- grails-app/controllers/aplicacaofinanceirarestful/EstadoController.groovy
+++ grails-app/controllers/aplicacaofinanceirarestful/EstadoController.groovy
@@ -11,6 +11,58 @@ class EstadoController {
     EstadoService estadoService
     MessageSource messageSource
 
+    /**
+     * @api {delete} /estado/:id Delete
+     * @apiGroup Estado
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do estado.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 204 No Content
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484224334405,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/estado/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um estado que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     {
+     *       "timestamp": 1484224424107,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Estado não encontrado.",
+     *       "path": "/estado/0"
+     *     }       
+     *
+     * @apiError Conflict O estado cujo identificador foi passado como parâmetro não pode ser excluído.
+     *
+     * @apiErrorExample Conflict
+     *     HTTP/1.1 409 Conflict
+     *     {
+     *       "timestamp": 1484224497234,
+     *       "status": 409,
+     *       "error": "Conflict",
+     *       "message": "O estado selecionado possui pelo menos uma cidade e, portanto, não pode ser excluído.",
+     *       "path": "/estado/1"
+     *     }
+     */
     def delete() {
         Estado estado = estadoService.findById(params.id as Long)
 
@@ -28,10 +80,128 @@ class EstadoController {
         render status: HttpStatus.NO_CONTENT
     }
 
+    /**
+     * @api {get} /estado List
+     * @apiGroup Estado
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiSuccess {Number} id        Identificador do estado.
+     * @apiSuccess {String} nome      Nome do estado.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     [
+     *       {
+     *         "id": 3,
+     *         "nome": "Espirito Santo"
+     *       },
+     *       {
+     *         "id": 2,
+     *         "nome": "Rio de Janeiro"
+     *       },
+     *       {
+     *         "id": 1,
+     *         "nome": "Sao Paulo"
+     *       }
+     *     ] 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484232706578,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/estado"
+     *     }
+     *
+     */
     def index() {
         respond estadoService.findAllOrderByNome()
     }
 
+    /**
+     * @api {post} /estado Save
+     * @apiGroup Estado
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} Content-Type  Tipo do conteúdo da requisição.
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "Content-Type":  application/json
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiExample {json} Exemplo de requisição
+     *     {
+     *       "nome": "Sao Paulo"
+     *     }
+     *     
+     * @apiSuccess {Number} id      Identificador do estado.
+     * @apiSuccess {String} nome    Nome do estado.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 201 Created
+     *     {
+     *       "id": 1,
+     *       "nome": "Sao Paulo"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484232706578,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/estado"
+     *     }
+     *
+     * @apiError UnprocessableEntity Erros de validação de dados ou violação de regras de negócio.
+     * 
+     * @apiErrorExample UnprocessableEntity
+     *     HTTP/1.1 422 UnprocessableEntity - "Campos obrigatórios não foram fornecidos."
+     *     
+     *     {
+     *       "timestamp": 1484224887178,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O nome fornecido não pode ser nulo."
+     *         }
+     *       ],
+     *       "path": "/estado"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O nome fornecido já pertence a outro estado."
+     *
+     *     {
+     *       "timestamp": 1484232914607,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O nome fornecido já pertence a outro estado."
+     *         }
+     *       ],
+     *       "path": "/estado"
+     *     }
+     */
     def save() {
         JsonSlurper jsonSlurper = new JsonSlurper()
         Estado estado = new Estado(jsonSlurper.parseText(request.JSON.toString()))
@@ -40,6 +210,54 @@ class EstadoController {
         respond estado, [status: HttpStatus.CREATED, view: 'show']
     }
 
+    /**
+     * @api {get} /estado/:id Show
+     * @apiGroup Estado
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do estado.
+     *
+     * @apiSuccess {Number} id        Identificador do estado.
+     * @apiSuccess {String} nome      Nome do estado.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "id": 1,
+     *       "nome": "Sao Paulo"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484225396181,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/estado/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um estado que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "timestamp": 1484225446800,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Estado não encontrado.",
+     *       "path": "/estado/0"
+     *     }        
+     */
     def show() {
         Estado estado = estadoService.findById(params.id as Long)
 
@@ -50,6 +268,92 @@ class EstadoController {
         }
     }
 
+    /**
+     * @api {put} /estado/:id Update
+     * @apiGroup Estado
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} Content-Type  Tipo do conteúdo da requisição.
+     * @apiHeader {String} nomeDeUsuario Nome do usuário que está realizando a requisição.
+     * @apiHeader {String} token         Token de acesso.
+     *
+     * @apiHeaderExample {String} Exemplo de cabeçalho:  
+     *     "Content-Type":  application/json
+     *     "nomeDeUsuario": admin
+     *     "token":         PXO9PmSpQwukqfjqg5ukbG3O1Hgl1yH0
+     *
+     * @apiParam {Number} id Identificador do estado.
+     *
+     * @apiParamExample {json} Exemplo de requisição
+     *     {
+     *       "nome": "Sao Paulo"   
+     *     }
+     *
+     * @apiSuccess {Number} id      Identificador do estado.
+     * @apiSuccess {String} nome    Nome do estado.
+     *
+     * @apiSuccessExample Resposta de sucesso
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "id": 1,
+     *       "nome": "Sao Paulo"
+     *     } 
+     *
+     * @apiError Unauthorized Usuário não autorizado a acessar o recurso solicitado.
+     *
+     * @apiErrorExample Unauthorized
+     *     HTTP/1.1 401 Unauthorized
+     *     {
+     *       "timestamp": 1484224814226,
+     *       "status": 401,
+     *       "error": "Unauthorized",
+     *       "message": "Usuário não autorizado a acessar o recurso solicitado.",
+     *       "path": "/estado/1"
+     *     }
+     *
+     * @apiError NotFound Não existe um estado que possua o identificador passado como parâmetro.
+     *
+     * @apiErrorExample NotFound
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "timestamp": 1484233284661,
+     *       "status": 404,
+     *       "error": "Not Found",
+     *       "message": "Estado não encontrado.",
+     *       "path": "/estado/0"
+     *     }
+     *
+     * @apiError UnprocessableEntity Erros de validação de dados ou violação de regras de negócio.
+     * 
+     * @apiErrorExample UnprocessableEntity
+     *     HTTP/1.1 422 UnprocessableEntity - "Campos obrigatórios não foram fornecidos."
+     *     
+     *     {
+     *       "timestamp": 1484224887178,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O nome fornecido não pode ser nulo."
+     *         }
+     *       ],
+     *       "path": "/estado/1"
+     *     }
+     *
+     *     HTTP/1.1 422 UnprocessableEntity - "O nome fornecido já pertence a outro estado."
+     *
+     *     {
+     *       "timestamp": 1484225086607,
+     *       "status": 422,
+     *       "error": "Unprocessable Entity",
+     *       "errors": [
+     *         {
+     *           "message": "O nome fornecido já pertence a outro estado."
+     *         }
+     *       ],
+     *       "path": "/estado/1"
+     *     }
+     */
     def update() {
         Estado estado = estadoService.findById(params.id as Long)
 
